cfb71518e378130dcd4ee21a92d8e6394a1ef644
Charlie Detviler <charlie@aprsworld.com>
2016-04-15 16:22:26 +0000

    Changed headers back to what they were so that cabu.php would show the correct information

--- cabu.php
+++ cabu.php
@@ -29,8 +29,9 @@ $headers = '<script language="javascript" type="text/javascript" src="js/date.js
 <script language="javascript" type="text/javascript" src="js/cookies.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
 <script language="javascript" type="text/javascript" src="js/jquery.flot.js"></script>
-<script> var station_id = "'.$station_id.'"; </script>
-<script language="javascript" type="text/javascript" src="js/rdlogger.js"></script><script type="text/javascript" src="js/jQueryRotate.2.2.js"></script>';
+<script> var station_id = "'.$serial_id.'"; var parent="'.$serial_id.'"; </script>
+<script language="javascript" type="text/javascript" src="js/cabu.js"></script>
+<script type="text/javascript" src="js/jQueryRotate.2.2.js"></script>';
 
 
 $head=$title=$deviceInfo["displayName"];
